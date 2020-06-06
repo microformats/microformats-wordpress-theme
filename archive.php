9b38fa64b1053d845c035b688f822c2d30ad582e
David Shanske <david@shanske.com>
2020-06-06 18:23:01 +0000

    Replace custom archive title code with archive title function

--- archive.php
+++ archive.php
@@ -1,31 +1,7 @@
 <?php get_header(); ?>
 	<div id="content">
-		<?php if ( have_posts() ) : ?>
-
-			<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-			<?php /* If this is a category archive */ if ( is_category() ) { ?>				
-		<h2 class="pagetitle">Blog Archive for the '<?php echo single_cat_title(); ?>' Category</h2>
-		
-	  <?php /* If this is a daily archive */ } elseif ( is_day() ) { ?>
-		<h2 class="pagetitle">Blog Archive for <?php the_time( 'F jS, Y' ); ?></h2>
-		
-	 <?php /* If this is a monthly archive */ } elseif ( is_month() ) { ?>
-		<h2 class="pagetitle">Blog Archive for <?php the_time( 'F, Y' ); ?></h2>
-
-		<?php /* If this is a yearly archive */ } elseif ( is_year() ) { ?>
-		<h2 class="pagetitle">Blog Archive for <?php the_time( 'Y' ); ?></h2>
-		
-	  <?php /* If this is a search */ } elseif ( is_search() ) { ?>
-		<h2 class="pagetitle">Search Results</h2>
-		
-	  <?php /* If this is an author archive */ } elseif ( is_author() ) { ?>
-		<h2 class="pagetitle">Author Archive</h2>
-
-		<?php /* If this is a paged archive */ } elseif ( isset( $_GET['paged'] ) && ! empty( $_GET['paged'] ) ) { ?>
-		<h2 class="pagetitle">Blog Archive</h2>
-
-		<?php } ?>
-
+		<?php if ( have_posts() ) { ?>
+			<?php the_archive_title( '<h2 class="pagetitle">', '</h2>' ); ?>
 
 		<div class="navigation">
 			<div class="alignleft"><?php posts_nav_link( '', '', '&laquo; Previous Entries' ); ?></div>
@@ -52,13 +28,12 @@
 			<div class="alignleft"><?php posts_nav_link( '', '', '&laquo; Previous Entries' ); ?></div>
 			<div class="alignright"><?php posts_nav_link( '', 'Next Entries &raquo;', '' ); ?></div>
 		</div>
-	
-	<?php else : ?>
+	<?php } else { ?>
 
 		<h2 class="center">Not Found</h2>
-		<?php include TEMPLATEPATH . '/searchform.php'; ?>
+			<?php include TEMPLATEPATH . '/searchform.php'; ?>
 
-	<?php endif; ?>
+	<?php } ?>
 		
 	</div>
 
