ef070683eec2016678df47b2112669052bacb66c
David Shanske <david@shanske.com>
2020-06-06 21:00:43 +0000

    Redo page formatting to add post class

--- page.php
+++ page.php
@@ -6,30 +6,11 @@
 	if ( have_posts() ) :
 		while ( have_posts() ) :
 			the_post();
-			?>
-		<div class="post">
-		<h2 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h2>
-			<div class="entrytext">
-				<?php the_content( '<p class="serif">Read the rest of this page &raquo;</p>' ); ?>
-	
-				<?php
-				wp_link_pages(
-					array(
-						'before'         => '<p><strong>Pages:</strong> ',
-						'after'          => '</p>',
-						'next_or_number' => 'number',
-					)
-				);
-				?>
-	
-			</div>
-		</div>
-			  <?php
+			get_template_part( 'template-parts/content', 'page' );
 	  endwhile;
 endif;
 	?>
-	<?php edit_post_link( 'Edit this entry.', '<p>', '</p>' ); ?>
-	</div>
+</div>
 
 <?php get_sidebar(); ?>
 
