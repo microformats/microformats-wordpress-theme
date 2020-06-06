c953d2ebdd6a8ba80859065de0be77b1981402f9
David Shanske <david@shanske.com>
2020-06-06 18:11:48 +0000

    Style Fixes courtesy phpcbf

--- footer.php
+++ footer.php
@@ -2,11 +2,11 @@
 
 <div id="footer">
 	<p>Powered by <a href="http://wordpress.org">WordPress</a> | <a href="http://mediatemple.net/">(mt) media temple</a> | <a href="http://no-www.org/">No WWW</a>.	
-		<!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. -->
+		<!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop( 1 ); ?> seconds. -->
 	</p>
 </div>
 
 </div> <!-- end #wrap -->
-<?php do_action('wp_footer'); ?>
+<?php do_action( 'wp_footer' ); ?>
 </body>
 </html>
--- search.php
+++ search.php
@@ -2,39 +2,42 @@
 
 	<div id="content" class="narrowcolumn">
 
-	<?php if (have_posts()) : ?>
+	<?php if ( have_posts() ) : ?>
 
 		<h2 class="pagetitle">Search Results</h2>
 		
 		<div class="navigation">
-			<div class="alignleft"><?php posts_nav_link('','','&laquo; Previous Entries') ?></div>
-			<div class="alignright"><?php posts_nav_link('','Next Entries &raquo;','') ?></div>
+			<div class="alignleft"><?php posts_nav_link( '', '', '&laquo; Previous Entries' ); ?></div>
+			<div class="alignright"><?php posts_nav_link( '', 'Next Entries &raquo;', '' ); ?></div>
 		</div>
 
 
-		<?php while (have_posts()) : the_post(); ?>
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			?>
 				
 			<div class="entry">
-				<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
+				<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
 				<?php the_excerpt(); ?>
 				<ul class="post-info">
-					<li><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_time('F jS, Y') ?></a></li>
-					<li><address class="vcard"><a class="url fn" href="<?php the_author_url() ?>"><?php the_author('namefl'); ?></a></address></li>
-					<li><?php comments_popup_link('Add Comment', '1 Comment', '% Comments'); ?></li>
+					<li><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_time( 'F jS, Y' ); ?></a></li>
+					<li><address class="vcard"><a class="url fn" href="<?php the_author_url(); ?>"><?php the_author( 'namefl' ); ?></a></address></li>
+					<li><?php comments_popup_link( 'Add Comment', '1 Comment', '% Comments' ); ?></li>
 				</ul>
 			</div>
 	
 		<?php endwhile; ?>
 
 		<div class="navigation">
-			<div class="alignleft"><?php posts_nav_link('','','&laquo; Previous Entries') ?></div>
-			<div class="alignright"><?php posts_nav_link('','Next Entries &raquo;','') ?></div>
+			<div class="alignleft"><?php posts_nav_link( '', '', '&laquo; Previous Entries' ); ?></div>
+			<div class="alignright"><?php posts_nav_link( '', 'Next Entries &raquo;', '' ); ?></div>
 		</div>
 	
 	<?php else : ?>
 
 		<h2 class="center">Not Found</h2>
-		<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+		<?php include TEMPLATEPATH . '/searchform.php'; ?>
 
 	<?php endif; ?>
 		
