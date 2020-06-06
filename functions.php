3d3babf2f729b2c2082c5ea0a20e227262d97858
David Shanske <david@shanske.com>
2020-06-06 20:27:34 +0000

    Let WordPress manage title field and properly register widgets with description

--- functions.php
+++ functions.php
@@ -1,12 +1,24 @@
 <?php
 
-
-// This theme uses wp_nav_menu() in for the top menu.
+function microformatsorg_theme_setup() {
+	// This theme uses wp_nav_menu() in for the top menu.
 	register_nav_menus(
 		array(
 			'primary' => __( 'Primary Menu', 'microformats' ),
 		)
 	);
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
 
 	/*
 	 * Switch default core markup for search form, comment form, and comments
@@ -22,11 +34,16 @@
 			'caption',
 		)
 	);
+}
+add_action( 'after_setup_theme', 'microformatsorg_theme_setup' );
 
+function microformatsorg_widget_init() {
 	if ( function_exists( 'register_sidebar' ) ) {
 		register_sidebar(
 			array(
 				'name'          => 'Global Sidebar',
+				'id'            => 'global-sidebar',
+				'description'   => 'Sidebar appears on all pages',
 				'before_widget' => '<div id="%1$s" class="box widget %2$s"><div class="box-inner">',
 				'after_widget'  => '</div></div>',
 				'before_title'  => '<h3>',
@@ -37,6 +54,8 @@
 	register_sidebar(
 		array(
 			'name'          => 'Home Page',
+			'id'            => 'home-page',
+			'description'   => 'Sidebar only appears on home page',
 			'before_widget' => '<div id="%1$s" class="box widget %2$s"><div class="box-inner">',
 			'after_widget'  => '</div></div>',
 			'before_title'  => '<h3>',
@@ -46,10 +65,14 @@
 	register_sidebar(
 		array(
 			'name'          => 'Post Page',
+			'id'            => 'post-page',
+			'description'   => 'Sidebar only appears on post pages',
 			'before_widget' => '<div id="%1$s" class="box widget %2$s"><div class="box-inner">',
 			'after_widget'  => '</div></div>',
 			'before_title'  => '<h3>',
 			'after_title'   => '</h3>',
 		)
 	);
+}
+add_action( 'widgets_init', 'microformatsorg_widget_init' );
 
--- header.php
+++ header.php
@@ -1,22 +1,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" <?php language_attributes(); ?>>
 <head profile="http://gmpg.org/xfn/11">
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title><?php bloginfo( 'name' ); ?> <?php
-	if ( is_single() ) {
-		?>
-		 | weblog 
-								   <?php
-	}
-	?>
-			<?php wp_title( '|' ); ?></title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<link rel="stylesheet" type="text/css" media="screen, projection" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 	<link rel="stylesheet" type="text/css" media="print" href="<?php bloginfo( 'stylesheet_directory' ); ?>/css/print.css" />
 	<link rel="shortcut icon" type="image/ico" href="/favicon.ico" />
 	<?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 
 <div id="wrap">
 	<div id="header">
