ff2f5fa7bd451575e66e64d3d31837281928332b
David Shanske <david@shanske.com>
2020-06-06 20:47:30 +0000

    Remove mediatemple link

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <hr class="hide" />
 
 <div id="footer">
-	<p>Powered by <a href="http://wordpress.org">WordPress</a> | <a href="http://mediatemple.net/">(mt) media temple</a> | <a href="http://no-www.org/">No WWW</a>.	
+	<p>Powered by <a href="http://wordpress.org">WordPress</a> | <a href="http://no-www.org/">No WWW</a>.	
 		<!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop( 1 ); ?> seconds. -->
 	</p>
 </div>
