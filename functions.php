cae391c4b4d02311fbead6b79e9c72f2c9a7dbdc
David Shanske <david@shanske.com>
2020-06-06 18:11:02 +0000

    Replace bookmarks with menu system

--- functions.php
+++ functions.php
@@ -1,24 +1,55 @@
 <?php
-if ( function_exists('register_sidebar') )
-    register_sidebar(array(
-        'name' => 'Global Sidebar',
-        'before_widget' => '<div id="%1$s" class="box widget %2$s"><div class="box-inner">',
-        'after_widget' => '</div></div>',
-        'before_title' => '<h3>',
-        'after_title' => '</h3>',
-    ));
-    register_sidebar(array(
-        'name' => 'Home Page',
-        'before_widget' => '<div id="%1$s" class="box widget %2$s"><div class="box-inner">',
-        'after_widget' => '</div></div>',
-        'before_title' => '<h3>',
-        'after_title' => '</h3>',
-    ));
-    register_sidebar(array(
-        'name' => 'Post Page',
-        'before_widget' => '<div id="%1$s" class="box widget %2$s"><div class="box-inner">',
-        'after_widget' => '</div></div>',
-        'before_title' => '<h3>',
-        'after_title' => '</h3>',
-    ));
-?>
+
+
+// This theme uses wp_nav_menu() in for the top menu.
+	register_nav_menus(
+		array(
+			'primary' => __( 'Primary Menu', 'microformats' ),
+		)
+	);
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		)
+	);
+
+	if ( function_exists( 'register_sidebar' ) ) {
+		register_sidebar(
+			array(
+				'name'          => 'Global Sidebar',
+				'before_widget' => '<div id="%1$s" class="box widget %2$s"><div class="box-inner">',
+				'after_widget'  => '</div></div>',
+				'before_title'  => '<h3>',
+				'after_title'   => '</h3>',
+			)
+		);
+	}
+	register_sidebar(
+		array(
+			'name'          => 'Home Page',
+			'before_widget' => '<div id="%1$s" class="box widget %2$s"><div class="box-inner">',
+			'after_widget'  => '</div></div>',
+			'before_title'  => '<h3>',
+			'after_title'   => '</h3>',
+		)
+	);
+	register_sidebar(
+		array(
+			'name'          => 'Post Page',
+			'before_widget' => '<div id="%1$s" class="box widget %2$s"><div class="box-inner">',
+			'after_widget'  => '</div></div>',
+			'before_title'  => '<h3>',
+			'after_title'   => '</h3>',
+		)
+	);
+
