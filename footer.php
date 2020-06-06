f9b6082f1560ef4c49dd574804de4e8db27f2fa4
David Shanske <david@shanske.com>
2020-06-06 20:47:59 +0000

    Remove hidden query counter

--- footer.php
+++ footer.php
@@ -2,7 +2,6 @@
 
 <div id="footer">
 	<p>Powered by <a href="http://wordpress.org">WordPress</a> | <a href="http://no-www.org/">No WWW</a>.	
-		<!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop( 1 ); ?> seconds. -->
 	</p>
 </div>
 
